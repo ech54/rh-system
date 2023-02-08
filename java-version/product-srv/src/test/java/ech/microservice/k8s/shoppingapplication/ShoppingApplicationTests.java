1f1f8340ed40c610490a0e95d2b7fa5ce3895247
Emilien Charton <emiliencharton@MacBook-Air-de-Emilien.local>
2023-02-08 15:10:29 +0100

    prepare project

---
+++ java-version/product-srv/src/test/java/ech/microservice/k8s/shoppingapplication/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package ech.microservice.k8s.shoppingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
